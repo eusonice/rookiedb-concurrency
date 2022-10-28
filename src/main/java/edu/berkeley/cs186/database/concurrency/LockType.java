dad5269e9627e3e21bb81073a45dd0b0c20b8a15
Eunice <eunice.leee@gmail.com>
2022-10-27 21:25:19 -0700

    find matrix patterns

--- src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
+++ src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
@@ -17,12 +17,37 @@ public enum LockType {
      * at the same time another transaction holds lock type B on the same
      * resource, the lock types are compatible.
      */
+    /**
+     *     | NL  | IS  | IX  |  S  | SIX |  X
+     * ----+-----+-----+-----+-----+-----+-----
+     * NL  |  T  |  T  |  T  |  T  |  T  |  T
+     * ----+-----+-----+-----+-----+-----+-----
+     * IS  |  T  |  T  |  T  |  T  |  T  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * IX  |  T  |  T  |  T  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * S   |  T  |  T  |  F  |  T  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * SIX |  T  |  T  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * X   |  T  |  F  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     */
     public static boolean compatible(LockType a, LockType b) {
         if (a == null || b == null) {
             throw new NullPointerException("null lock type");
         }
-        // TODO(proj4_part1): implement
-
+        if (a == NL || b == NL) {
+            return true;
+        } else if (a == X || b == X) {
+            return false;
+        } else if (a == IS || b == IS) {
+            return true;
+        } else if (a == SIX || b == SIX) {
+            return false;
+        } else if (a == b) {
+            return true;
+        }
         return false;
     }
 
@@ -49,12 +74,41 @@ public enum LockType {
      * This method returns if parentLockType has permissions to grant a childLockType
      * on a child.
      */
+    /**
+     *     | NL  | IS  | IX  |  S  | SIX |  X
+     * ----+-----+-----+-----+-----+-----+-----
+     * NL  |  T  |  F  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * IS  |  T  |  T  |  F  |  T  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * IX  |  T  |  T  |  T  |  T  |  T  |  T
+     * ----+-----+-----+-----+-----+-----+-----
+     * S   |  T  |  F  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * SIX |  T  |  F  |  T  |  F  |  F  |  T
+     * ----+-----+-----+-----+-----+-----+-----
+     * X   |  T  |  F  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     */
     public static boolean canBeParentLock(LockType parentLockType, LockType childLockType) {
         if (parentLockType == null || childLockType == null) {
             throw new NullPointerException("null lock type");
         }
-        // TODO(proj4_part1): implement
-
+        if (childLockType == NL) {
+            return true;
+        } else if (parentLockType == NL) {
+            return false;
+        } else if (parentLockType == X) {
+            return false;
+        } else if (parentLockType == IX) {
+            return true;
+        } else if (parentLockType == S) {
+            return false;
+        } else if (parentLockType == parentLock(childLockType)) {
+            return true;
+        } else if (parentLockType == SIX && (childLockType == IX || childLockType == X)) {
+            return true;
+        }
         return false;
     }
 
@@ -64,12 +118,39 @@ public enum LockType {
      * an X lock, because an X lock allows the transaction to do everything
      * the S lock allowed it to do).
      */
+    /**
+     *     | NL  | IS  | IX  |  S  | SIX |  X
+     * ----+-----+-----+-----+-----+-----+-----
+     * NL  |  T  |  F  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * IS  |  T  |  T  |  F  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * IX  |  T  |  T  |  T  |  F  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * S   |  T  |  F  |  F  |  T  |  F  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * SIX |  T  |  F  |  F  |  T  |  T  |  F
+     * ----+-----+-----+-----+-----+-----+-----
+     * X   |  T  |  F  |  F  |  T  |  F  |  T
+     * ----+-----+-----+-----+-----+-----+-----
+     */
     public static boolean substitutable(LockType substitute, LockType required) {
         if (required == null || substitute == null) {
             throw new NullPointerException("null lock type");
         }
-        // TODO(proj4_part1): implement
-
+        if (substitute == required) {
+            return true;
+        } else if (substitute == NL) {
+            return false;
+        } else if (required == NL) {
+            return true;
+        } else if (substitute == IX && required == IS) {
+            return true;
+        } else if (substitute == SIX && required == S) {
+            return true;
+        } else if (substitute == X && required == S) {
+            return true;
+        }
         return false;
     }
 
